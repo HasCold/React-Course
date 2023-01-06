99772da3cf1534ff7acf61a8c35c92aa4ebc4708
Hasan <ha03330224926@gmail.com>
2023-01-06 22:47:00 +0500

    Update React Router Directory

--- src/React_Router/App.js
+++ src/React_Router/App.js
@@ -12,11 +12,13 @@ import Error from "./components/Error.jsx";
 
 // For the use of React Router, You have to take guidance and reference from the official documentation 'reactrouter.com'. This will help you to route your application with complete explanation.
 
+// https://dev.to/adidoshi/react-router-dom-v6-51j2 ; It is very helpful link to take reference regarding the react-router-dom current version with the complete advanced explanation;
+
 function App(){
   
     return (
         //  Is tarah se hum navigate karnenge aur hamari application dubara reload nahi hogi khali hamara ak component render hoga  
-    
+
     <Router>    {/* <Router> </Router>  */}  
     <div>
     <Header />
--- src/React_Router/components/Header.js
+++ src/React_Router/components/Header.js
@@ -1,15 +1,25 @@
-import { Link } from "react-router-dom"
+import { Link, NavLink } from "react-router-dom"
 
 const Header = () => {
+  
+    const navLinkStyles = ({ isActive }) => {
+      return {
+        fontWeight: isActive ? "bold" : "normal",
+        // textDecoration: isActive ? "none" : "underline",
+        color: isActive ? "red" : "white"
+      };
+    }
+
   return (
     <div className="header">
     <h1 style={{cursor: "pointer"}}>React Router</h1>
 
                       {/* Link :- <Link></Link> */}
                       {/* Is tarah se hum navigate karnenge aur hamari application dubara reload nahi hogi khali hamara ak component render hoga  */}
-    <Link to={"/Home"}>   
+    <div className="info">
+    <NavLink style={navLinkStyles} to={"/home"}>   
     <h2 style={{cursor: "pointer"}}>Home</h2>      
-      </Link>
+      </NavLink>
 
       <Link to={"/About"}>
     <h2 style={{cursor: "pointer"}}>About</h2>      
@@ -18,6 +28,7 @@ const Header = () => {
       <Link to={"/Contact"}>      
     <h2 style={{cursor: "pointer"}}>Contact</h2>
       </Link>
+    </div>
     </div>
   )
 }
